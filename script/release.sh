a8c6b63a1c296d90d2f4e8c80de45d4780940f9b
bob <bob.bouteillier@datadoghq.com>
2021-08-11 17:31:27 +0200

    Cross build / release

--- script/release.sh
+++ script/release.sh
@@ -31,14 +31,17 @@ function build_project() {
 	# so download, install and build against it.
 
 	local libseccomp_ver='2.5.1'
-	local tarball="libseccomp-${libseccomp_ver}.tar.gz"
 	local prefix
+	local arch=x86_64
+	local strip=${STRIP:-strip}
+	[[ ${goarch} == "arm64" ]] && arch=${CC%%-*}
+	local tarball="libseccomp-${libseccomp_ver}.tar.gz"
 	prefix="$(mktemp -d)"
 	wget "https://github.com/seccomp/libseccomp/releases/download/v${libseccomp_ver}/${tarball}"{,.asc}
 	tar xf "$tarball"
 	(
 		cd "libseccomp-${libseccomp_ver}"
-		./configure --prefix="$prefix" --enable-static --disable-shared
+		./configure --prefix="$prefix" --enable-static --disable-shared --build=x86_64 --host="${arch}" --target="${arch}"
 		make install
 	)
 	mv "$tarball"{,.asc} "$builddir"
@@ -53,7 +56,7 @@ function build_project() {
 	# it can reuse cached pkg-config results).
 	make -C "$root" PKG_CONFIG_PATH="${prefix}/lib/pkgconfig" COMMIT_NO= EXTRA_FLAGS="-a" EXTRA_LDFLAGS="${ldflags}" static
 	rm -rf "$prefix"
-	strip "$root/$project"
+	${strip} "$root/$project"
 	mv "$root/$project" "$1"
 }
 
@@ -122,6 +125,10 @@ version="${version:-$(<"$root/VERSION")}"
 releasedir="${releasedir:-release/$version}"
 hashcmd="${hashcmd:-sha256sum}"
 goarch="$(go env GOARCH || echo "amd64")"
+CI=${CI:-false}
+if [[ ${CI} = true ]]; then
+	releasedir="release/$goarch"
+fi
 
 log "creating $project release in '$releasedir'"
 log "  version: $version"
@@ -139,12 +146,12 @@ rm -rf "$releasedir" && mkdir -p "$releasedir"
 build_project "$releasedir/$project.$goarch"
 
 # Generate new archive.
-git archive --format=tar --prefix="$project-$version/" "$commit" | xz >"$releasedir/$project.tar.xz"
+git archive --format=tar --prefix="$project-$version/" "$commit" | xz >"$releasedir/$project.$goarch.tar.xz"
 
 # Generate sha256 checksums for both.
 (
 	cd "$releasedir"
-	"$hashcmd" "$project".{"$goarch",tar.xz} >"$project.$hashcmd"
+	"$hashcmd" "$project.$goarch"{,.tar.xz} >"$project.$goarch.$hashcmd"
 )
 
 # Set up the gpgflags.
